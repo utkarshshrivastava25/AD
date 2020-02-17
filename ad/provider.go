52f822e693b67dd1dee86d8c442e280ea6dd26ae
utkarshshrivastava25 <60725471+utkarshshrivastava25@users.noreply.github.com>
2020-02-17 06:20:52 +0530

    Update provider.go

--- ad/provider.go
+++ ad/provider.go
@@ -54,7 +54,7 @@ func Provider() terraform.ResourceProvider {
 	}
 }
 
-func providerConfigure(d *schema.ResourceData) (interface{}, error) {
+func providerConfigure(d *schema.ResourceData) (interface{}, error) {         //returns a client that will communicate with the API
 
 	config := Config{
 		Domain:   d.Get("domain").(string),
